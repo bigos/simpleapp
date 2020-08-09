18ebb90960df81ec6fd861900edef31a609afcbc
Jacek Podkanski <ruby.object@googlemail.com>
2020-08-09 01:02:57 +0100

    unsuccessful attempt to create simple clojure app

--- src/simpleapp/core.clj
+++ src/simpleapp/core.clj
@@ -1,7 +1,7 @@
 (ns simpleapp.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer :all]
+
             [org.httpkit.server :refer [run-server]]))
 
 ;; https://learnxinyminutes.com/docs/compojure/
@@ -9,10 +9,8 @@
 ;; curl -X POST http://localhost:5000/generate/abc.xml/def.pdf
 
 (defroutes app
+  (GET "/generate*" {params :query-params} (str params))
   (GET "/" [] "<h1>Hello</h1>")
-  (POST "/generate" req (let [iff (get (:params req) :iff)
-                              off (get (:params req) :off)]
-                          (str "iff: " iff ", off: " off)))
   (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main
